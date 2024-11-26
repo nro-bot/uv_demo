e6bfd269d6f406e4938514d7d0d3f30a0ab8cb33
nouyang <nouyang@alum.mit.edu>
2024-11-25 20:09:05 -0800

    refactored mocks into utils

---
+++ utils.py
@@ -0,0 +1,77 @@
+import numpy as np
+import pandas as pd
+from phenoxtractor import find_gleasons
+
+MOCKSID = 1231231231230
+
+def get_truth_df():
+    return df
+
+def get_text_df():
+    return df
+
+
+def get_mock_truth_df():
+    # make dummy df
+    df = pd.DataFrame(
+        [
+            [1010101010, 1231231231230, 25, 3, "Gleason_1"],
+            [1010101010, 1231231231230, 27, 4, "Gleason_2"],
+            [1010101010, 1231231231230, 29, 7, "Gleason_total"],
+            [1010101010, 1231231231230, 114, 3, "Gleason_1"],
+            [1010101010, 1231231231230, 117, 3, "Gleason_2"],
+            [1010101010, 1231231231230, 120, 6, "Gleason_total"],
+            [1010101010, 1231231231230, 159, 3, "Gleason_1"],
+            [1010101010, 1231231231230, 165, 4, "Gleason_2"],
+            [1010101010, 1231231231230, 168, 7, "Gleason_total"],
+        ],
+        columns=["PatientICN", "TextSID", "Start", "Text", "Label"],
+    ).astype({"Label": pd.StringDtype(), "Text": pd.StringDtype()})
+    return df
+
+
+def get_mock_text_df():
+    df = pd.DataFrame(
+        [
+            [
+                1010101010,
+                1231231231230,
+                (
+                    "Lorem ipsum there is a Gleason 3+4=7 in this text that we"
+                    "are looking at and in fact there may even be a second"
+                    "gleason 6 (3 +3) score in the text with a gleason of 3"
+                    "and 4 (7/10)"
+                ),
+            ]
+        ],
+        columns=["PatientICN", "TIUDocumentSID", "ReportText"],
+    )
+    return df
+
+
+def get_mock_results_df():
+    results = []
+    text_df = get_mock_text_df()
+    for i, row in text_df.iterrows():
+        #    print(row)
+        #    print(find_gleasons(row.ReportText))
+        gl = find_gleasons(row.ReportText)
+        gl = [[MOCKSID] + x for x in gl]
+        results.extend(gl)
+
+    results.append([MOCKSID, 1, "4", "Gleason_1", "mock text"])
+    results.append([MOCKSID, 3, "4", "Gleason_2", "mock text"])
+    results.append([MOCKSID, 5, "8", "Gleason_total", "mock text"])
+
+    df = pd.DataFrame(
+        np.array(results), columns=["TextSID", "Start", "Text", "Label", "Context"]
+    ).astype(
+        {
+            "TextSID": int,
+            "Start": int,
+            "Text": pd.StringDtype(),
+            "Label": pd.StringDtype(),
+            "Context": pd.StringDtype(),
+        }
+    )
+    return df
